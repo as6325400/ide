556b722eadbc4df91c40aa43139bf9e267ca4c46
as6325400 <as6325400@gmail.com>
2024-10-29 19:19:09 +0800

    add docker

--- js/ide.js
+++ js/ide.js
@@ -7,6 +7,8 @@ const AUTH_HEADERS = API_KEY ? {
 var defaultUrl = localStorageGetItem("api-url") || "https://judge0-ce.p.rapidapi.com";
 var extraApiUrl = "https://judge0-extra-ce.p.rapidapi.com";
 
+// defaultUrl = "";
+
 if (location.hostname == "ide.judge0.com") {
     defaultUrl = "https://ce.judge0.com";
     extraApiUrl = "https://extra-ce.judge0.com";
